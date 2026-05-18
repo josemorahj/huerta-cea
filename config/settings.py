1f70a751af6b4a23059c9502ae56b42b7e7f7f15
Jose Mora <https://github.com/josemorahj>
2026-05-17 22:06:31 -0400

    fix: add DEFAULT_AUTO_FIELD and fix migrations

--- config/settings.py
+++ config/settings.py
@@ -85,6 +85,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
